32f5108d014052fe66142b44350be958aa862654
mehran <mehrtarh1@gmail.com>
2020-08-24 12:14:49 +0430

    first commit

---
+++ MyCocoapodsLibrary/Classes/Logger.swift
@@ -0,0 +1,25 @@
+//
+//  Logger.swift
+//  MyCocoapodsLibrary
+//
+//  Created by Mehran on 6/3/1399 AP.
+//
+
+import Foundation
+
+public class Logger
+{
+   public init() {
+    
+    }
+    
+   public func printLog()
+    {
+        print("Hello Word");
+    }
+    
+     func privateMethod()
+    {
+        print("private");
+    }
+}
